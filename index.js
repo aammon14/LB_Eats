1be51322c74d89a90be19afc96d973e46611eaf4
Andrew Ammon <andrewammon@ool-ad020367.dyn.optonline.net>
2018-01-26 13:56:12 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const morgan = require("morgan");
+const mustacheExpress = require('mustache-express');
+
+
+const restaurantsRouter = require('./controllers/restaurants');
+
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.engine('html', mustacheExpress());
+app.set('view engine', 'html');
+app.set('views', __dirname + '/views');
+app.use(express.static(__dirname + '/public'));
+
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+  res.render('main');
+});
+
+app.use('/restaurants', restaurantsRouter);
+
+app.use((err, req, res, next) => {
+  console.log('Error encountered:', err);
+  res.status(500);
+  res.send(err);
+});
+
+app.listen(PORT, () => { console.log("Server started on " + PORT); });
